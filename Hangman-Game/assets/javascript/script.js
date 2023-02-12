0b7519393305ecc9b00cd457c61f41173ae657e3
jamesdosstech <jamesdosstech@gmail.com>
2023-02-12 17:04:54 -0600

    fixed random word bug

--- Hangman-Game/assets/javascript/script.js
+++ Hangman-Game/assets/javascript/script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    let wordList = ['Skywalker', 'Stormtrooper'];
+    let wordList = ['Skywalker', 'Stormtrooper', 'Amidala'];
 
     //declarations
     //html
@@ -9,36 +9,34 @@ window.onload = function() {
     let displayedScore = document.getElementById('score');
     let displayedLetters = document.getElementById('letters');
     let displayedRules = document.getElementById('rules');
-    //alphabet
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     let lives = 5;
     let counter = 0;
     let correctLetters = [];
     let wrongLetters = []
-
+    let word;
     let lossVoiceLine = new Audio('assets/images/your_father.mp3');
     let winVoiceLine = new Audio('assets/images/forcestrong.mp3');
 
     //display start button
     start.innerHTML = '<button>Start</button>';
-    
-    //testing
-    let test = 'testing '
 
-    // random word
-    let word = wordList[this.Math.floor(this.Math.random() * wordList.length)].toLocaleLowerCase();
-    // replace letters of word with underscore
-    for (i = 0; i < word.length; i++) {
-        console.log(correctLetters[i]);
-        correctLetters[i] = '_';
+    const randomizeWord = () => {
+        correctLetters = [];
+        word = wordList[Math.floor(Math.random() * wordList.length)].toLocaleLowerCase();
+        console.log('randomized word ' + word)
+        // replace letters of word with underscore
+        for (i = 0; i < word.length; i++) {
+            correctLetters[i] = '_';
+        }
+        console.log('this is the word ' + word)
     }
-
     //losing conditon
     
     // check letters
     const checkLetters = () => {
         document.onkeydown = (e) => {
             // guess letter
+            console.log('check letters ' + word, correctLetters)
             let userGuess = e.key.toLocaleLowerCase();
             let found = false;
             for(i = 0; i < word.length; i++) {
@@ -49,29 +47,47 @@ window.onload = function() {
                     displayedWord.innerHTML = correctLetters.join(' ')
                 }
             }
-            if (found) return;
-            // if wrong letter send it to wrong letter bank and subtract lives
-            if (wrongLetters.indexOf(userGuess) < 0){
+            if (found) {
+                winLossCondition();
+                counter++;
+                displayedScore.innerHTML = `Score Board: ${counter}`;
+            } else if (!found) {
+                winLossCondition();
                 wrongLetters.push(userGuess);
                 displayedLetters.innerHTML = `Wrong Letters: ${wrongLetters.join(' ')}`;
                 lives--;
                 displayedLives.innerHTML = `Lives Left: ${lives}`;
-            }
+                console.log(`lives ${lives--}`);
+            };
+        }    
+    }
+
+    const winLossCondition = (a) => {
+        if(lives > 0 && correctLetters.join('') === word ) {
+            console.log('win');
+            displayedWord.innerHTML = 'you win';
+            return;
+            // randomizeWord();
+        }else if (lives === 0) {
+            console.log('loss')
         }
     }
 
     const game = () => {
-        checkLetters()
+        // randomizeWord();
+        checkLetters();
     }
 
     //functions
     const startGame = (b) => {
-        game();
+        randomizeWord();
+        game(word, correctLetters);
+        // displayedWord.innerHTML = '';
         displayedWord.innerHTML = b.join(' ');
         displayedLetters.innerHTML = `Wrong Letters: `;
         displayedRules.innerHTML = 'Type Any Letter and Attempt to Guess The Correct Star Wars Name'
-        displayedLives.innerHTML = `Lives Left: ${lives}`;
-        displayedScore.innerHTML = 'Score Board: ';
+        displayedLives.innerHTML = `Lives Left: ${lives = 5}`;
+        displayedScore.innerHTML = `Score Board: ${counter = 0}`;
         // console.log('guessed letters' + e)
 
         // game();
@@ -79,6 +95,6 @@ window.onload = function() {
     
     //event listeners
     start.addEventListener('click', function() {
-        startGame(correctLetters, lives)
+        startGame(correctLetters)
     })
 }
