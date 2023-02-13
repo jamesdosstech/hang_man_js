8eeadff333eed50cabade6a9bc835140a1719809
James Doss <jamesdoss@Jamess-MBP-2.attlocal.net>
2023-02-12 20:48:42 -0600

    finished game need media styles

--- Hangman-Game/assets/javascript/script.js
+++ Hangman-Game/assets/javascript/script.js
@@ -1,6 +1,5 @@
 window.onload = function() {
     let wordList = ['Skywalker', 'Stormtrooper', 'Amidala'];
-
     //declarations
     //html
     const start = document.getElementById('button');
@@ -9,6 +8,7 @@ window.onload = function() {
     let displayedScore = document.getElementById('score');
     let displayedLetters = document.getElementById('letters');
     let displayedRules = document.getElementById('rules');
+    let displayedImage = document.getElementById('image');
     let lives = 5;
     let counter = 0;
     let correctLetters = [];
@@ -18,7 +18,7 @@ window.onload = function() {
     const winVoiceLine = new Audio('assets/images/forcestrong.mp3');
 
     //display start button
-    start.innerHTML = '<button>Start</button>';
+    start.innerHTML = '<button class="button-container">Start</button>';
 
     const randomizeWord = () => {
         // correctLetters = [];
@@ -30,6 +30,23 @@ window.onload = function() {
         }
         console.log('this is the word ' + word)
     }
+
+    const changeImage = () => {
+        console.log(`life count ${lives}`)
+        if(lives == 5) {
+            displayedImage.innerHTML = "<img src='./assets/images/start_image.jpg' />";
+        }if(lives == 4) {
+            displayedImage.innerHTML = "<img src='./assets/images/one_limb.jpg' />";
+        }if(lives == 3) {
+            displayedImage.innerHTML = "<img src='./assets/images/limb_2_ready.jpg' />";
+        }if(lives == 2) {
+            displayedImage.innerHTML = "<img src='./assets/images/limb_2_gone.gif' />";
+        }if(lives == 1) {
+            displayedImage.innerHTML = "<img src='./assets/images/darth_vader_1.jpg' />";
+        }if(lives == 0) {
+            displayedImage.innerHTML = "<img src='./assets/images/darth_vader_2.jpg' />";
+        }
+    }
     
     // check letters
     const checkLetters = () => {
@@ -38,6 +55,7 @@ window.onload = function() {
                 //if button is visibil no input will trigger
                 return
             } else {
+                // changeImage();
                 // guess letter
                 let userGuess = e.key.toLocaleLowerCase();
                 let found = false;
@@ -47,18 +65,20 @@ window.onload = function() {
                         console.log('userguess ' + userGuess)
                         correctLetters[i] = userGuess;
                         found = true;
-                        displayedWord.innerHTML = correctLetters.join(' ')
+                        displayedWord.innerHTML = correctLetters.join(' ');
                     }
                 }
                 if (found) {
                     winLossCondition();
                     counter++;
+                    changeImage();
                     displayedScore.innerHTML = `Score Board: ${counter}`;
                 } else if (!found) {
                     winLossCondition(e);
                     wrongLetters.push(userGuess);
                     displayedLetters.innerHTML = `Wrong Letters: ${wrongLetters.join(' ')}`;
                     lives -= 1;
+                    changeImage();
                     displayedLives.innerHTML = `Lives Left: ${lives}`;
                 };
             }
@@ -70,7 +90,7 @@ window.onload = function() {
             correctLetters = [];
             winVoiceLine.play();
             displayedWord.innerHTML = 'you win';
-            start.innerHTML = '<button>Next</button>';
+            start.innerHTML = '<button class="button-container">Next</button>';
             wrongLetters = [];
             winVoiceLine.play();
             // randomizeWord();
@@ -80,24 +100,29 @@ window.onload = function() {
             correctLetters = [];
             counter = 0;
             lives = 6;
-            start.innerHTML = '<button>Restart</button>';
+            start.innerHTML = '<button class="button-container">Restart</button>';
             lossVoiceLine.play();
         }
     }
 
     const game = () => {
         // randomizeWord();
-        console.log(start.innerHTML);
         checkLetters();
     }
 
     //functions
     const startGame = (b) => {
         randomizeWord();
+        changeImage();
         game(word, correctLetters);
         start.innerHTML = '';
         wrongLetters = [];
         // displayedWord.innerHTML = '';
+        displayedWord.classList.add('container');
+        displayedLetters.classList.add('container');
+        displayedLives.classList.add('container');
+        displayedRules.classList.add('container');
+        displayedScore.classList.add('container');
         displayedWord.innerHTML = b.join(' ');
         displayedLetters.innerHTML = `Wrong Letters: `;
         displayedRules.innerHTML = 'Type Any Letter and Attempt to Guess The Correct Star Wars Name'
